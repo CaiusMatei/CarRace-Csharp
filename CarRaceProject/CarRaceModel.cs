2677fc81aa4aca84eabd20415127631036b1d11d
Caius Matei <caius.matei@chasacademy.se>
2023-05-06 21:42:41 +0200

    finish product

---
+++ CarRaceProject/CarRaceModel.cs
@@ -0,0 +1,20 @@
+﻿namespace CarRaceProject
+{
+    public class CarRaceModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Speed { get; set; }
+        public int TimeSec { get; set; }
+        public double DistanceKm { get; set; }
+        public int PenaltyTimer { get; set; }
+        public int PreviousChance { get; set; }
+
+        public double RemainingDistance(double racelength)
+        {
+
+            return racelength - DistanceKm;
+        }
+
+    }
+}
